e68df12f87babf2a9be205cf1ca507f5776c511b
Vascal <vascal2018@gmail.com>
2019-04-25 22:14:57 +0800

    fix: throw self-defined exception occur error #8 (#19)

--- chaosblade-exec-common/src/main/java/com/alibaba/chaosblade/exec/common/model/action/exception/DefaultThrowExceptionExecutor.java
+++ chaosblade-exec-common/src/main/java/com/alibaba/chaosblade/exec/common/model/action/exception/DefaultThrowExceptionExecutor.java
@@ -62,8 +62,14 @@ public class DefaultThrowExceptionExecutor implements ThrowExceptionExecutor {
         try {
             Class<?> clazz = classLoader.loadClass(exception);
             if (Exception.class.isAssignableFrom(clazz)) {
-                Constructor<?> constructor = clazz.getConstructor(String.class);
-                return (Exception)constructor.newInstance("chaosblade-mock-exception");
+            	Constructor<?>[] constructors = clazz.getConstructors();
+    			for(Constructor<?> constructor: constructors) {
+    				if(constructor.getParameterTypes().length == 0) 
+    					return (Exception)constructor.newInstance();
+    				else if (constructor.getParameterTypes().length == 1 && constructor.getParameterTypes()[0].getName().equals("java.lang.String")) 
+    	                return (Exception)constructor.newInstance("chaosblade-mock-exception");
+    			}
+                return new RuntimeException("Failed to instantiate exception: "+ exception +", no default or single-string-param constructor found.");
             }
             return new RuntimeException(exception + " not assign from java.lang.Exception");
         } catch (Throwable e) {
@@ -88,8 +94,14 @@ public class DefaultThrowExceptionExecutor implements ThrowExceptionExecutor {
         Class<?> exceptionType = exceptionTypes[0];
         try {
             if (Exception.class.isAssignableFrom(exceptionType)) {
-                Constructor<?> constructor = exceptionType.getConstructor(String.class);
-                return (Exception)constructor.newInstance("chaosblade-mock-exception");
+            	Constructor<?>[] constructors = exceptionType.getConstructors();
+    			for(Constructor<?> constructor: constructors) {
+    				if(constructor.getParameterTypes().length == 0) 
+    					return (Exception)constructor.newInstance();
+    				else if (constructor.getParameterTypes().length == 1 && constructor.getParameterTypes()[0].getName().equals("java.lang.String")) 
+    	                return (Exception)constructor.newInstance("chaosblade-mock-exception");
+    			}
+                return new RuntimeException("Failed to instantiate exception: "+ exceptionType.getName() +", no default or single-string-param constructor found.");
             }
             return new RuntimeException("the " + exceptionType.getName() + " not assign from java.lang.Exception");
         } catch (Throwable e) {
