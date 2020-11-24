6313fb747514d7744b2f445db89da8fb8fa8f790
xueshaoyi <545038292@qq.com>
2020-11-24 20:36:17 +0800

    modify

--- chaosblade-exec-plugin/chaosblade-exec-plugin-redisson/src/main/java/com/alibaba/chaosblade/exec/plugin/redisson/RedissonPointCut.java
+++ chaosblade-exec-plugin/chaosblade-exec-plugin-redisson/src/main/java/com/alibaba/chaosblade/exec/plugin/redisson/RedissonPointCut.java
@@ -21,8 +21,6 @@ import com.alibaba.chaosblade.exec.common.aop.PointCut;
 import com.alibaba.chaosblade.exec.common.aop.matcher.clazz.ClassMatcher;
 import com.alibaba.chaosblade.exec.common.aop.matcher.clazz.NameClassMatcher;
 import com.alibaba.chaosblade.exec.common.aop.matcher.clazz.OrClassMatcher;
-import com.alibaba.chaosblade.exec.common.aop.matcher.clazz.SuperClassMatcher;
-import com.alibaba.chaosblade.exec.common.aop.matcher.method.ManyNameMethodMatcher;
 import com.alibaba.chaosblade.exec.common.aop.matcher.method.MethodMatcher;
 import com.alibaba.chaosblade.exec.common.aop.matcher.method.NameMethodMatcher;
 
