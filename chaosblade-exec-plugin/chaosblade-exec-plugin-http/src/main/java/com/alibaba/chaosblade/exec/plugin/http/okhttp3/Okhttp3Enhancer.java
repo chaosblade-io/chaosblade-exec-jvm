da67d4e095a024f01188db9ebcefc4e3e6337d0d
zhucegep <zhucegep@gmail.com>
2021-06-20 14:19:39 +0800

    - http故障模拟支持async-http-client. - 各类httpclient支持超时演练：async-http-client、httpclient3、httpclient4、okhttp3、resttemplate. - 修复http plugin的EnhancerModel设置错误问题.
    Signed-off-by: shizhi.zhu <shizhi.zhu@qunar.com>

--- chaosblade-exec-plugin/chaosblade-exec-plugin-http/src/main/java/com/alibaba/chaosblade/exec/plugin/http/okhttp3/Okhttp3Enhancer.java
+++ chaosblade-exec-plugin/chaosblade-exec-plugin-http/src/main/java/com/alibaba/chaosblade/exec/plugin/http/okhttp3/Okhttp3Enhancer.java
@@ -1,18 +1,13 @@
 package com.alibaba.chaosblade.exec.plugin.http.okhttp3;
 
-import java.lang.reflect.Method;
-
-import com.alibaba.chaosblade.exec.common.aop.BeforeEnhancer;
 import com.alibaba.chaosblade.exec.common.aop.EnhancerModel;
-import com.alibaba.chaosblade.exec.common.model.matcher.MatcherModel;
-import com.alibaba.chaosblade.exec.common.util.JsonUtil;
 import com.alibaba.chaosblade.exec.common.util.ReflectUtil;
-import com.alibaba.chaosblade.exec.plugin.http.HttpConstant;
+import com.alibaba.chaosblade.exec.plugin.http.HttpEnhancer;
 import com.alibaba.chaosblade.exec.plugin.http.UrlUtils;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import static com.alibaba.chaosblade.exec.plugin.http.HttpConstant.DEFAULT_TIMEOUT;
 import static com.alibaba.chaosblade.exec.plugin.http.HttpConstant.OKHTTP3;
 
 /**
@@ -21,36 +16,42 @@ import static com.alibaba.chaosblade.exec.plugin.http.HttpConstant.OKHTTP3;
  * @author pengpj
  * @date 2020-7-13
  */
-public class Okhttp3Enhancer extends BeforeEnhancer {
+public class Okhttp3Enhancer extends HttpEnhancer {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(Okhttp3Enhancer.class);
 
     private static final String GET_REQUEST = "request";
     private static final String GET_URL = "url";
+    private static final String GET_CONNECTION_TIMEOUT = "connectTimeoutMillis";
+    private static final String GET_READ_TIMEOUT = "readTimeoutMillis";
 
-    /**
-     * okhttp3.RealCall 的 url ，从成员变量 originalRequest 中获取
-     */
+
     @Override
-    public EnhancerModel doBeforeAdvice(ClassLoader classLoader,
-                                        String className,
-                                        Object object,
-                                        Method method,
-                                        Object[] methodArguments) throws Exception {
-        MatcherModel matcherModel = new MatcherModel();
-        matcherModel.add(HttpConstant.URI_KEY, getUrl(object));
-        if (LOGGER.isDebugEnabled()) {
-            LOGGER.debug("http matchers: {}", JsonUtil.writer().writeValueAsString(matcherModel));
-        }
-        EnhancerModel enhancerModel = new EnhancerModel(classLoader, matcherModel);
+    protected void postDoBeforeAdvice(EnhancerModel enhancerModel) {
         enhancerModel.addMatcher(OKHTTP3, "true");
-        return new EnhancerModel(classLoader, matcherModel);
+    }
+
+    protected int getTimeout(Object instance, Object[] methodArguments) {
+        try {
+            Object client = ReflectUtil.getFieldValue(instance, "client", false);
+            if (client == null) {
+                LOGGER.warn("OkHttpClient from Call not found. return default value {}", DEFAULT_TIMEOUT);
+                return DEFAULT_TIMEOUT;
+            }
+            int connectionTimeout = ReflectUtil.invokeMethod(client, GET_CONNECTION_TIMEOUT, new Object[0], false);
+            int readTimeout = ReflectUtil.invokeMethod(client, GET_READ_TIMEOUT, new Object[0], false);
+            return connectionTimeout + readTimeout;
+        } catch (Exception e) {
+            LOGGER.warn("Getting timeout from url occurs exception. return default value {}", DEFAULT_TIMEOUT, e);
+        }
+        return DEFAULT_TIMEOUT;
     }
 
     /**
      * get url
      */
-    private String getUrl(Object realCall) throws Exception {
+    @Override
+    protected String getUrl(Object realCall, Object[] object) throws Exception {
         Object request = ReflectUtil.invokeMethod(realCall, GET_REQUEST, new Object[0], false);
         if (request == null) {
             LOGGER.warn("okhttp3 Request is null, can not get necessary values.");
--- chaosblade-exec-plugin/chaosblade-exec-plugin-http/src/main/java/com/alibaba/chaosblade/exec/plugin/http/resttemplate/RestTemplateEnhancer.java
+++ chaosblade-exec-plugin/chaosblade-exec-plugin-http/src/main/java/com/alibaba/chaosblade/exec/plugin/http/resttemplate/RestTemplateEnhancer.java
@@ -1,9 +1,14 @@
 package com.alibaba.chaosblade.exec.plugin.http.resttemplate;
 
 import com.alibaba.chaosblade.exec.common.aop.EnhancerModel;
+import com.alibaba.chaosblade.exec.common.util.ReflectUtil;
 import com.alibaba.chaosblade.exec.plugin.http.HttpConstant;
 import com.alibaba.chaosblade.exec.plugin.http.HttpEnhancer;
 import com.alibaba.chaosblade.exec.plugin.http.UrlUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import static com.alibaba.chaosblade.exec.plugin.http.HttpConstant.DEFAULT_TIMEOUT;
 
 /**
  * @Author yuhan
@@ -11,6 +16,7 @@ import com.alibaba.chaosblade.exec.plugin.http.UrlUtils;
  * @Date 2019-05-08 20:23
  */
 public class RestTemplateEnhancer extends HttpEnhancer {
+    private static final Logger LOGGER = LoggerFactory.getLogger(RestTemplateEnhancer.class);
 
     @Override
     protected void postDoBeforeAdvice(EnhancerModel enhancerModel) {
@@ -18,12 +24,29 @@ public class RestTemplateEnhancer extends HttpEnhancer {
     }
 
     @Override
-    protected String getUrl(Object [] objects) {
-        if(null == objects){
+    protected int getTimeout(Object instance, Object[] methodArguments) {
+        try {
+            Object requestFactory = ReflectUtil.getSuperclassFieldValue(instance, "requestFactory", false);
+            if (requestFactory == null) {
+                LOGGER.warn("requestFactory from RestTemplate not found. return default value {}", DEFAULT_TIMEOUT);
+                return DEFAULT_TIMEOUT;
+            }
+            int connectionTimeout = ReflectUtil.getFieldValue(requestFactory, "connectTimeout", false);
+            int readTimeout = ReflectUtil.getFieldValue(requestFactory, "readTimeout", false);
+            return connectionTimeout + readTimeout;
+        } catch (Exception e) {
+            LOGGER.warn("Getting timeout from url occurs exception. return default value {}", DEFAULT_TIMEOUT, e);
+        }
+        return DEFAULT_TIMEOUT;
+    }
+
+    @Override
+    protected String getUrl(Object instance, Object[] objects) {
+        if (null == objects) {
             return null;
         }
         Object object = objects[0];
-        if(null == object){
+        if (null == object) {
             return null;
         }
         return UrlUtils.getUrlExcludeQueryParameters(object.toString());
