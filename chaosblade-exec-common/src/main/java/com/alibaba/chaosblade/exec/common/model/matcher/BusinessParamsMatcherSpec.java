9457c888bf75f43423d6a0f49beb538e0e38f397
wufan <wufunc@gmail.com>
2021-12-08 20:52:37 +0800

    optimize business param empty check
    Signed-off-by: wufan <wufunc@gmail.com>

--- chaosblade-exec-common/src/main/java/com/alibaba/chaosblade/exec/common/model/matcher/BusinessParamsMatcherSpec.java
+++ chaosblade-exec-common/src/main/java/com/alibaba/chaosblade/exec/common/model/matcher/BusinessParamsMatcherSpec.java
@@ -3,6 +3,7 @@ package com.alibaba.chaosblade.exec.common.model.matcher;
 import com.alibaba.chaosblade.exec.common.aop.PredicateResult;
 import com.alibaba.chaosblade.exec.common.constant.ModelConstant;
 import com.alibaba.chaosblade.exec.common.util.BusinessParamUtil;
+import com.alibaba.chaosblade.exec.common.util.StringUtil;
 import com.alibaba.chaosblade.exec.common.util.StringUtils;
 
 import java.util.List;
@@ -34,12 +35,15 @@ public class BusinessParamsMatcherSpec extends BasePredicateMatcherSpec {
     @Override
     public PredicateResult predicate(MatcherModel matcherModel) {
         String bParam = matcherModel.get(ModelConstant.BUSINESS_PARAMS);
-        if (StringUtils.isEmpty(bParam)) {
-            return PredicateResult.success();
-        }
-        List<BusinessParamUtil.BusinessParam> params = BusinessParamUtil.parseFromJsonStr(bParam);
-        if (params == null || params.isEmpty()) {
-            return PredicateResult.fail(getName() + " illegal json");
+        if (StringUtils.isBlank(bParam)) {
+            if (required()) {
+                return PredicateResult.fail("less necessary " + getName() + " value");
+            }
+        } else {
+            List<BusinessParamUtil.BusinessParam> params = BusinessParamUtil.parseFromJsonStr(bParam);
+            if (params == null || params.isEmpty()) {
+                return PredicateResult.fail(getName() + " illegal json");
+            }
         }
         return PredicateResult.success();
     }
