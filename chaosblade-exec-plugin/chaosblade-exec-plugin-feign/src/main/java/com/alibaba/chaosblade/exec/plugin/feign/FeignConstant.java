2bbf1b2d968c2fa89252bae74deefbdd675ec4b9
hengyu-coder <guoyu486@gmail.com>
2022-06-06 17:58:13 +0800

    commit spring feign plugin
    Signed-off-by: hengyu-coder <guoyu486@gmail.com>

---
+++ chaosblade-exec-plugin/chaosblade-exec-plugin-feign/src/main/java/com/alibaba/chaosblade/exec/plugin/feign/FeignConstant.java
@@ -0,0 +1,18 @@
+package com.alibaba.chaosblade.exec.plugin.feign;
+
+/**
+ * @author guoyu486@gmail.com
+ */
+public interface FeignConstant {
+
+    String TARGET_NAME = "feign";
+
+    String ENHANCER_CLASS = "feign.SynchronousMethodHandler";
+
+    String METHOD = "executeAndDecode";
+
+    String SERVICE_NAME = "service-name";
+
+    String TEMPLATE_URL = "url";
+
+}
