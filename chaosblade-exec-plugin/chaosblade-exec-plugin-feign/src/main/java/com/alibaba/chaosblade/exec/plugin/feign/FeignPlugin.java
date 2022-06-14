bf193ddde5cf774149a90fadac836226ec2ce35b
hengyu-coder <guoyu486@gmail.com>
2022-06-14 16:15:47 +0800

    commit feign plugins

---
+++ chaosblade-exec-plugin/chaosblade-exec-plugin-feign/src/main/java/com/alibaba/chaosblade/exec/plugin/feign/FeignPlugin.java
@@ -0,0 +1,33 @@
+package com.alibaba.chaosblade.exec.plugin.feign;
+
+import com.alibaba.chaosblade.exec.common.aop.Enhancer;
+import com.alibaba.chaosblade.exec.common.aop.Plugin;
+import com.alibaba.chaosblade.exec.common.aop.PointCut;
+import com.alibaba.chaosblade.exec.common.model.ModelSpec;
+import com.alibaba.chaosblade.exec.plugin.feign.model.FeignModelSpec;
+
+/**
+ * @author guoyu486@gmail.com
+ */
+public class FeignPlugin implements Plugin, FeignConstant {
+
+    @Override
+    public ModelSpec getModelSpec() {
+        return new FeignModelSpec();
+    }
+
+    @Override
+    public String getName() {
+        return "feign";
+    }
+
+    @Override
+    public PointCut getPointCut() {
+        return new FeignProducerPointCut();
+    }
+
+    @Override
+    public Enhancer getEnhancer() {
+        return new FeignProducerEnhancer();
+    }
+}
