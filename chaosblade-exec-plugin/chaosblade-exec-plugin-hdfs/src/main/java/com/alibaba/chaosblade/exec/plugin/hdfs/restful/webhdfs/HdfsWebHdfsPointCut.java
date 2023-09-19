d7a550de0e98c1c03c0e9ee7aff0b3df98b5b023
glindenh <glindenh@163.com>
2023-09-19 10:36:59 +0800

    refactor: optimize WehHDFS pointcut filter and its enhancer, add Apache License to all files.
    Signed-off-by: glindenh <glindenh@163.com>

--- chaosblade-exec-plugin/chaosblade-exec-plugin-hdfs/src/main/java/com/alibaba/chaosblade/exec/plugin/hdfs/restful/webhdfs/HdfsWebHdfsPointCut.java
+++ chaosblade-exec-plugin/chaosblade-exec-plugin-hdfs/src/main/java/com/alibaba/chaosblade/exec/plugin/hdfs/restful/webhdfs/HdfsWebHdfsPointCut.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 1999-2013 Alibaba Group Holding Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.alibaba.chaosblade.exec.plugin.hdfs.restful.webhdfs;
 
 import com.alibaba.chaosblade.exec.common.aop.PointCut;
@@ -5,15 +21,11 @@ import com.alibaba.chaosblade.exec.common.aop.matcher.MethodInfo;
 import com.alibaba.chaosblade.exec.common.aop.matcher.clazz.ClassMatcher;
 import com.alibaba.chaosblade.exec.common.aop.matcher.clazz.NameClassMatcher;
 import com.alibaba.chaosblade.exec.common.aop.matcher.method.MethodMatcher;
-import javassist.bytecode.AccessFlag;
 import java.util.Arrays;
 
 public class HdfsWebHdfsPointCut implements PointCut {
     // NameNode handler
     private static final String CLASS_HDFS_WEBHDFS_METHODS = "org.apache.hadoop.hdfs.server.namenode.web.resources.NamenodeWebHdfsMethods";
-
-    // DataNode handler, including operations such as OPEN, CREATE, APPEND, GETFILECHECKSUM, etc.
-    // private static final String CLASS_HDFS_WEBHDFS_HANDLER = "org.apache.hadoop.hdfs.server.datanode.web.webhdfs.WebHdfsHandler";
 
     private static final String METHOD_HDFS_WEBHDFS_GET = "get";
     private static final String METHOD_HDFS_WEBHDFS_PUT = "put";
@@ -33,8 +45,19 @@ public class HdfsWebHdfsPointCut implements PointCut {
 
     @Override
     public MethodMatcher getMethodMatcher() {
-        return (String methodName, MethodInfo methodInfo) ->
-                Arrays.asList(TARGET_METHODS).contains(methodName)
-                && (methodInfo.getAccess() & AccessFlag.PUBLIC) == AccessFlag.PUBLIC;
+        MethodMatcher methodMatcher = new MethodMatcher() {
+            @Override
+            public boolean isMatched(String methodName, MethodInfo methodInfo) {
+                String[] parameterTypes = methodInfo.getParameterTypes();
+                if (parameterTypes == null || parameterTypes.length < 5) {
+                    return false;
+                }
+
+                return Arrays.asList(TARGET_METHODS).contains(methodName)
+                       && String.class.getTypeName().equals(methodInfo.getParameterTypes()[4]);
+            }
+        };
+
+        return methodMatcher;
     }
 }
