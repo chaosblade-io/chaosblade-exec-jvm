7dbc01c5e25887718e37785570a9edc5e32f1f3c
glindenh <glindenh@163.com>
2023-09-19 10:36:59 +0800

    refactor: optimize WehHDFS pointcut filter and its enhancer, add Apache License to all files.
    Signed-off-by: glindenh <glindenh@163.com>

--- chaosblade-exec-plugin/chaosblade-exec-plugin-hdfs/src/main/java/com/alibaba/chaosblade/exec/plugin/hdfs/restful/webhdfs/HdfsWebHdfsEnhancer.java
+++ chaosblade-exec-plugin/chaosblade-exec-plugin-hdfs/src/main/java/com/alibaba/chaosblade/exec/plugin/hdfs/restful/webhdfs/HdfsWebHdfsEnhancer.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 1999-2013 Alibaba Group Holding Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.alibaba.chaosblade.exec.plugin.hdfs.restful.webhdfs;
 
 import com.alibaba.chaosblade.exec.common.aop.BeforeEnhancer;
@@ -20,7 +36,7 @@ public class HdfsWebHdfsEnhancer extends BeforeEnhancer {
                                         Object[] methodArguments) throws Exception {
         MatcherModel matcherModel = new MatcherModel();
         if (methodArguments[4] != null) {
-            String filePath = ReflectUtil.invokeMethod(methodArguments[4], "getAbsolutePath");
+            String filePath = String.valueOf(methodArguments[4]);
             if (StringUtils.isNotBlank(filePath)) {
                 matcherModel.add(HdfsConstant.FLAG_COMMON_FILE, filePath.trim());
             }
